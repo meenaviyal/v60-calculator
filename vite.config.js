52a0f7379e9b7ac2360e6fb247d4ec719e7f9a9c
Hiran Venugopalan <hiran.v@gmail.com>
2025-02-13 13:23:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tetsu-calculator/', // Replace with your repo name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
