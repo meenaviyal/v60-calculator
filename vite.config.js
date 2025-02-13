b16731e5deabc8f44a9166812834fa9cd0415d13
Hiran Venugopalan <hiran.v@gmail.com>
2025-02-13 13:24:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/v60-calculator
+  base: '/v60-calculator',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
